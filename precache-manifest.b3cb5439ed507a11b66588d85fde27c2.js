6641f8b3690bedf871e1c95e1bb36229c71254c3
Mizraim <mizraim.martinez@gmail.com>
2019-03-15 16:58:55 -0600

    Updates

---
+++ precache-manifest.b3cb5439ed507a11b66588d85fde27c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "615e1117b99de4099a66",
+    "url": "/cotizacion-motos/static/css/main.bfd06a07.chunk.css"
+  },
+  {
+    "revision": "615e1117b99de4099a66",
+    "url": "/cotizacion-motos/static/js/main.2c0ec7e1.chunk.js"
+  },
+  {
+    "revision": "10373acf0b56208e9195",
+    "url": "/cotizacion-motos/static/js/runtime~main.d1d7495c.js"
+  },
+  {
+    "revision": "32fa736f81feb37849a6",
+    "url": "/cotizacion-motos/static/js/2.cb2dea98.chunk.js"
+  },
+  {
+    "revision": "9774813e01f8eb8b779a45239bf0dc9c",
+    "url": "/cotizacion-motos/index.html"
+  }
+];
--- src/Component/as.js
+++
@@ -1,7 +0,0 @@
-{/*<th className="text-center">Modelo</th>*/}
-{/*<th className="text-center">  Precio</th>*/}
-{/*<th className="text-center">Frenos</th>*/}
-{/*<th className="text-center">Peso</th>*/}
-{/*<th className="text-center">Largo</th>*/}
-{/*<th className="text-center">Url</th>*/}
-{/*<th className="text-center">Image</th>*/}
